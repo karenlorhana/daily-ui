d38886876199b58c6a6cfbda5d51e746b1a4797f
Karen Lorhana <karen10lorhana@gmail.com>
2021-09-02 12:43:53 -0300

    New

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/day001.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
